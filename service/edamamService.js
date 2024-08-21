8c9157cf63f5f7260a655d9b7e3890417f8af5ab
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-21 12:00:03 +1000

    Improve code

--- service/edamamService.js
+++ service/edamamService.js
@@ -3,23 +3,31 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+// hard-coded query parameter for backend development stage
 const query = 'chicken'
 
 const fetchRecipes = async () => {
-    const url = `https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+    // Construct the URL for the Edamam API request
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}` 
 
     try {
+        // Send a GET request to the Edamam API
         const response = await fetch(url);
+
+        // Check if the response is OK (status code 200-299)
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
+        // Parse and return the JSON response from the API
         return await response.json();
     } catch (err) {
+        // Log any errors that occur during the fetch process
         console.log('Error fetching data from Edamam API:', err);
     }
 }
 
-// // Testing the function
+// Testing the function in the terminal:
 // const testFetchRecipes = async () => {
 //     try {
 //         const data = await fetchRecipes();

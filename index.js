af2c48a5fe9765805381fcd0bf7f512b10b11742
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-08-30 16:52:13 +1000

    test: added app.js and app.test.js for the tests

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-import express from 'express'
-import recipeRoutes from './controllers/recipeController.js'
-import cors from 'cors'
-
-const app = express();
-app.use(cors()) 
-app.use(express.json())
-
-// Define a root route
-app.get('/', (req, res) => {
-    res.send('Welcome to Diet-Delight API');
-});
-
-// Mount recipe routes to handle requests related to recipes
-app.use('/recipes', recipeRoutes);
+import app from './app.js'
 
 app.listen(4001, err => {
     if (err) {

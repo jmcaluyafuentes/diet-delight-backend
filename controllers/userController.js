6190b5968157d0f22a109c1f7b41456cb223c542
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-30 00:55:32 +1000

    feat(user): create endpoint to delete all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,32 +5,6 @@ import User from '../models/User.js'
 
 const router = express.Router();
 
-router.post("/add-user", async(req, res) => {
-    const { username, password } = req.body;
-    
-    try {
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-        
-        const newUser = new User({
-            username: username,
-            password: password
-        })
-
-        await newUser.save();
-
-        res.status(201).json({ 
-            message: 'User created successfully',
-            username: newUser.username
-        });
-    } catch (error) {
-        console.error('Error adding user: ', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-});
-
 router.delete("/delete-user", async (req, res) => {
     const { username } = req.body;
     
@@ -62,7 +36,7 @@ router.post("/register", async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         
         const newUser = new User({
-            username: username,
+            username,
             password: hashedPassword
         })
 
@@ -81,10 +55,31 @@ router.post("/register", async (req, res) => {
 router.get('/all-users',  async (req, res) => {
     try {
         const users = await User.find();
-        res.status(200).json(users.map((user) => user.username));
 
+        if (!users.length) {
+            return res.status(404).json({ message: 'No users found'})
+        }
+
+        res.status(200).json(users.map((user) => user.username));
     } catch (error) {
-        console.error('Error retrieving all users');
+        console.error('Error retrieving all users', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+router.delete('/all-users', async (req, res) => {
+    try {
+        const users = await User.find();
+
+        if (!users.length) {
+            return res.status(404).json({ message: 'No users found' });
+        }
+
+        await User.deleteMany();
+
+        res.status(200).json({ message: 'All users successfully deleted'})
+    } catch (error) {
+        console.error('Error deleting the users', error);
         res.status(500).json({ message: 'Internal server error' })
     }
 })
